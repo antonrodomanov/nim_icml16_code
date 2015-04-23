07898db5720772c90a8c3e54e8105ea208210c00
Anton Rodomanov <anton.rodomanov@gmail.com>
2015-04-23 13:40:20 +0300

    Add command line option parser.

--- main.cc
+++ main.cc
@@ -12,8 +12,93 @@
 #include "optim.h"
 #include "logger.h"
 
-int main()
+#include "optionparser.h"
+
+struct Arg: public option::Arg
 {
+  static void printError(const char* msg1, const option::Option& opt, const char* msg2)
+  {
+    fprintf(stderr, "%s", msg1);
+    fwrite(opt.name, opt.namelen, 1, stderr);
+    fprintf(stderr, "%s", msg2);
+  }
+
+  static option::ArgStatus Unknown(const option::Option& option, bool msg)
+  {
+    if (msg) printError("Unknown option '", option, "'\n");
+    return option::ARG_ILLEGAL;
+  }
+
+  static option::ArgStatus Required(const option::Option& option, bool msg)
+  {
+    if (option.arg != 0)
+      return option::ARG_OK;
+
+    if (msg) printError("Option '", option, "' requires an argument\n");
+    return option::ARG_ILLEGAL;
+  }
+
+  static option::ArgStatus NonEmpty(const option::Option& option, bool msg)
+  {
+    if (option.arg != 0 && option.arg[0] != 0)
+      return option::ARG_OK;
+
+    if (msg) printError("Option '", option, "' requires a non-empty argument\n");
+    return option::ARG_ILLEGAL;
+  }
+
+  static option::ArgStatus Numeric(const option::Option& option, bool msg)
+  {
+    char* endptr = 0;
+    if (option.arg != 0 && strtol(option.arg, &endptr, 10)){};
+    if (endptr != option.arg && *endptr == 0)
+      return option::ARG_OK;
+
+    if (msg) printError("Option '", option, "' requires a numeric argument\n");
+    return option::ARG_ILLEGAL;
+  }
+};
+
+enum optionIndex { UNKNOWN, HELP, METHOD, MAX_EPOCHS };
+const option::Descriptor usage[] =
+{
+    {UNKNOWN, 0, "" , "", option::Arg::None, "USAGE: ./main [options]\n\n"
+                                             "Options:" },
+    {HELP, 0, "", "help", option::Arg::None, "  --help  \tPrint usage and exit." },
+    {METHOD, 0, "", "method", Arg::Required, "  --method \tOptimisation method (SAG, SGD, SO2)." },
+    {MAX_EPOCHS, 0, "", "max_epochs", Arg::Required, "  --max_epochs \tMaximum number of epochs." },
+    {0,0,0,0,0,0}
+};
+
+int main(int argc, char* argv[])
+{
+    argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
+    option::Stats stats(usage, argc, argv);
+    option::Option options[stats.options_max], buffer[stats.buffer_max];
+    option::Parser parse(usage, argc, argv, options, buffer);
+
+    if (parse.error())
+        return 1;
+
+    if (options[HELP] || argc == 0) {
+        option::printUsage(std::cout, usage);
+        return 0;
+    }
+
+    std::string method = options[METHOD].arg;
+
+    if (!options[MAX_EPOCHS]) {
+        fprintf(stderr, "Require max_epochs\n");
+        return 1;
+    }
+
+    double max_epochs;
+    sscanf(options[MAX_EPOCHS].arg, "%lf", &max_epochs);
+
+    /* ========================================================================== */
+    /* ========================================================================== */
+    /* ========================================================================== */
+
     std::vector<std::vector<double>> Z;
     std::vector<int> y;
     double lambda;
@@ -33,15 +118,34 @@ int main()
     LogRegOracle func(Z, lambda);
     std::vector<double> w0 = std::vector<double>(Z[0].size(), 0.0);
 
-    double alpha = 1e-4;
-    int maxiter = 50 * Z.size();
-    Logger logger = SGD(func, w0, alpha, maxiter);
-    //Logger logger = SAG(func, w0, alpha, maxiter);
-    //Logger logger = SO2(func, w0, maxiter);
+    int maxiter = max_epochs * Z.size();
 
-    printf("%9s %9s %15s %15s\n", "epoch", "elapsed", "val", "norm_grad");
-    for (int i = 0; i < int(logger.trace_epoch.size()); ++i) {
-        printf("%9.2f %9.2f %15.6e %15.6e\n", logger.trace_epoch[i], logger.trace_elaps[i], logger.trace_val[i], logger.trace_norm_grad[i]);
+    if (method == "SAG") {
+        Logger logger = SAG(func, w0, alpha, maxiter);
+
+        printf("%9s %9s %15s %15s\n", "epoch", "elapsed", "val", "norm_grad");
+        for (int i = 0; i < int(logger.trace_epoch.size()); ++i) {
+            printf("%9.2f %9.2f %15.6e %15.6e\n", logger.trace_epoch[i], logger.trace_elaps[i], logger.trace_val[i], logger.trace_norm_grad[i]);
+        }
+    } else if (method == "SGD") {
+        double alpha = 1e-4;
+
+        Logger logger = SGD(func, w0, alpha, maxiter);
+
+        printf("%9s %9s %15s %15s\n", "epoch", "elapsed", "val", "norm_grad");
+        for (int i = 0; i < int(logger.trace_epoch.size()); ++i) {
+            printf("%9.2f %9.2f %15.6e %15.6e\n", logger.trace_epoch[i], logger.trace_elaps[i], logger.trace_val[i], logger.trace_norm_grad[i]);
+        }
+    } else if (method == "SO2") {
+        Logger logger = SO2(func, w0, maxiter);
+
+        printf("%9s %9s %15s %15s\n", "epoch", "elapsed", "val", "norm_grad");
+        for (int i = 0; i < int(logger.trace_epoch.size()); ++i) {
+            printf("%9.2f %9.2f %15.6e %15.6e\n", logger.trace_epoch[i], logger.trace_elaps[i], logger.trace_val[i], logger.trace_norm_grad[i]);
+        }
+    } else {
+        fprintf(stderr, "Unknown method %s\n", method.c_str());
+        return 1;
     }
 
     return 0;
