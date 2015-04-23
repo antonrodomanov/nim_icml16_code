fb646570e621d79e678303f9b7122945313bf4a3
Anton Rodomanov <anton.rodomanov@gmail.com>
2015-04-23 14:01:10 +0300

    Add dataset covtype.

--- datasets.cc
+++ datasets.cc
@@ -23,3 +23,14 @@ void load_w8a(std::vector<std::vector<double>>& X, std::vector<int>& y)
     /* read data */
     read_svmlight_file("datasets/w8a", 49749 , 300, X, y);
 }
+
+void load_covtype(std::vector<std::vector<double>>& X, std::vector<int>& y)
+{
+    /* read data */
+    read_svmlight_file("datasets/covtype.libsvm.binary.scale", 581012 , 54, X, y);
+
+    /* transform y from {1, 2} to {-1, 1} */
+    for (int i = 0; i < int(y.size()); ++i) {
+        y[i] = (y[i] == 1) ? -1 : 1;
+    }
+}
