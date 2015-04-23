741a9b7466c5974e35c6a7ca728565fe7b0a317c
Anton Rodomanov <anton.rodomanov@gmail.com>
2015-04-23 03:14:14 +0300

    Add datasets.

---
+++ datasets.cc
@@ -0,0 +1,13 @@
+#include "datasets.h"
+#include "auxiliary.h"
+
+void load_mushrooms(std::vector<std::vector<double>>& X, std::vector<int>& y)
+{
+    /* read data */
+    read_svmlight_file("datasets/mushrooms", 8124, 112, X, y);
+
+    /* transform y from {1, 2} to {-1, 1} */
+    for (int i = 0; i < int(y.size()); ++i) {
+        y[i] = (y[i] == 1) ? -1 : 1;
+    }
+}
---
+++ datasets.h
@@ -0,0 +1,8 @@
+#ifndef DATASETS_H
+#define DATASETS_H
+
+#include <vector>
+
+void load_mushrooms(std::vector<std::vector<double>>& X, std::vector<int>& y);
+
+#endif
