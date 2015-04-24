264988850c3fb08c4601ab66ced2b7a432635512
Anton Rodomanov <anton.rodomanov@gmail.com>
2015-04-24 16:55:32 +0300

    Polish logger.

--- logger.cc
+++ logger.cc
@@ -1,49 +1,55 @@
-#include "logger.h"
+#include <iostream>
 #include <vector>
 #include <cstdio>
-#include "special.h"
 #include <ctime>
-#include <iostream>
 
-Logger::Logger(const LogRegOracle& func) :
+#include "logger.h"
+#include "special.h"
+
+Logger::Logger(const LogRegOracle& func, double n_logs_per_epoch) :
     func(func),
-    how_often(func.n_samples() / 10),
+    how_often(func.n_samples() / n_logs_per_epoch),
     n_calls(0),
     t_start(clock()),
     mainten_time(0.0)
-    {}
+{}
 
 void Logger::log(const Eigen::VectorXd& w)
 {
-    if (n_calls % how_often == 0) {
-        /* for counting time spent on this maintenance code */
+    if (n_calls % how_often == 0) { /* perform actual logging only every `how_often` calls */
+        /* remember the time when the maintenance starts */
         clock_t mainten_start = clock();
 
-        double epoch = double(n_calls) / func.n_samples();
-        double f = func.full_val(w);
-        Eigen::VectorXd g = func.full_grad(w);
-        double norm_g = g.lpNorm<Eigen::Infinity>();
-        double elaps = double(clock() - t_start) / CLOCKS_PER_SEC;
-        /* this code is just for maintenance, don't add its time to the optimiser's time */
-        elaps -= mainten_time;
-
-        /* always start elaps at zero, first call is for free */
+        /* calculate the number of epoch and optimiser's time */
+        double epoch = double(n_calls) / func.n_samples(); // current epoch
+        double tot_elaps = double(clock() - t_start) / CLOCKS_PER_SEC; // total time elapsed since optimiser was run
+        double opt_elaps = tot_elaps - mainten_time; // `tot_elaps` equals optimiser's time + maintenance time, so
+                                                     // subtract maintenance time to get optimiser's time
+        /* always start `opt_elaps` at zero (first call is for free) */
         if (n_calls == 0) {
-            elaps = 0;
+            opt_elaps = 0.0;
         }
 
-        /* print header */
-        if (n_calls == 0) {
+        /* calculate function value and gradient norm */
+        double f = func.full_val(w);
+        double norm_g = func.full_grad(w).lpNorm<Eigen::Infinity>();
+
+        /* print calculated values */
+        if (n_calls == 0) { /* print table header when it's the first call */
             fprintf(stderr, "%9s %9s %9s %15s %15s\n", "epoch", "opt_elaps", "tot_elaps", "f", "norm_g");
         }
-        fprintf(stderr, "%9.2f %9.2f %9.2f %15.6e %15.6e\n", epoch, elaps, elaps + mainten_time, f, norm_g);
+        fprintf(stderr, "%9.2f %9.2f %9.2f %15.6e %15.6e\n", epoch, opt_elaps, tot_elaps, f, norm_g);
 
+        /* append calculated values to trace */
         trace_epoch.push_back(epoch);
-        trace_elaps.push_back(elaps);
+        trace_elaps.push_back(opt_elaps);
         trace_val.push_back(f);
         trace_norm_grad.push_back(norm_g);
 
+        /* increase total maintenance time by the time spent on running this chunk of code */
         mainten_time += double(clock() - mainten_start) / CLOCKS_PER_SEC;
     }
+
+    /* increase total number of calls for this logger */
     ++n_calls;
 }
--- logger.h
+++ logger.h
@@ -1,28 +1,34 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include "LogRegOracle.h"
 #include <ctime>
 
 #include <Eigen/Dense>
 
+#include "LogRegOracle.h"
+
 class Logger {
 public:
-    Logger(const LogRegOracle& func);
+    /* Arguments:
+        func = the function being traced
+        n_logs_per_epoch = the number of requested logs per epoch (the larger this value, the more frequent the logs are)
+    */
+    Logger(const LogRegOracle& func, double n_logs_per_epoch=10.0);
 
     void log(const Eigen::VectorXd& w);
 
-    std::vector<double> trace_epoch;
-    std::vector<double> trace_elaps;
-    std::vector<double> trace_val;
-    std::vector<double> trace_norm_grad;
+    std::vector<double> trace_epoch; // epoch number
+    std::vector<double> trace_elaps; // elapsed time
+    std::vector<double> trace_val; // function value
+    std::vector<double> trace_norm_grad; // gradient norm
 
 private:
-    const LogRegOracle& func;
-    const int how_often; /* frequency of logging, in number of calls */
-    int n_calls;
-    clock_t t_start; /* time of creation of this logger */
-    double mainten_time; /* total time spent on maintenance tasks */
+    const LogRegOracle& func; // the function being traced
+    const int how_often; // frequency of logging, in number of calls
+
+    int n_calls; // number of times this logger has been called
+    clock_t t_start; // time of creation of this logger
+    double mainten_time; // total time spent on maintenance tasks
 };
 
 #endif
