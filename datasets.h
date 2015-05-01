44b5344dcb08c6a4133ec32ed612c3a3d1bbc33e
Anton Rodomanov <anton.rodomanov@gmail.com>
2015-05-01 22:17:18 +0300

    Add dataset protein.

--- datasets.h
+++ datasets.h
@@ -8,6 +8,7 @@ void load_a9a(Eigen::MatrixXd& X, Eigen::VectorXi& y);
 void load_w8a(Eigen::MatrixXd& X, Eigen::VectorXi& y);
 void load_covtype(Eigen::MatrixXd& X, Eigen::VectorXi& y);
 void load_quantum(Eigen::MatrixXd& X, Eigen::VectorXi& y);
+void load_protein(Eigen::MatrixXd& X, Eigen::VectorXi& y);
 void load_alpha(Eigen::MatrixXd& X, Eigen::VectorXi& y);
 void load_epsilon(Eigen::MatrixXd& X, Eigen::VectorXi& y);
 void load_zeta(Eigen::MatrixXd& X, Eigen::VectorXi& y);
--- main.cc
+++ main.cc
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
         );
         TCLAP::ValueArg<std::string> arg_dataset(
             "", "dataset",
-            "Dataset (a9a, mushrooms, w8a, covtype, quantum, alpha, beta, gamma, delta, epsilon, zeta)",
+            "Dataset (a9a, mushrooms, w8a, covtype, quantum, protein, alpha, beta, gamma, delta, epsilon, zeta)",
             true, dataset, "string"
         );
         TCLAP::ValueArg<double> arg_max_epochs(
@@ -113,6 +113,9 @@ int main(int argc, char* argv[])
     } else if (dataset == "quantum") {
         fprintf(stderr, "Load quantum\n");
         load_quantum(Z, y);
+    } else if (dataset == "protein") {
+        fprintf(stderr, "Load protein\n");
+        load_protein(Z, y);
     } else if (dataset == "alpha") {
         fprintf(stderr, "Load alpha, might take a lot of time\n");
         load_alpha(Z, y);
