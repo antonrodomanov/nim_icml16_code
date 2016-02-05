d97407016a5042f3308cc8a7a5a8035e39fc9cc6
Anton Rodomanov <anton.rodomanov@gmail.com>
2016-02-05 09:28:54 +0300

    Faster soft-thresholding.

--- special.cc
+++ special.cc
@@ -22,8 +22,18 @@ double sigm(double x)
 /* returns argmin_x (tau||x||_1 + 1/2||x - x_0||) */
 Eigen::VectorXd soft_threshold(const Eigen::VectorXd& x0, double tau)
 {
-    Eigen::VectorXd x = Eigen::VectorXd::Zero(x0.size());
-    x = (x0.array() > tau).select(x0.array() - tau, x);
-    x = (x0.array() < -tau).select(x0.array() + tau, x);
+    //Eigen::VectorXd x = Eigen::VectorXd::Zero(x0.size());
+    //x = (x0.array() > tau).select(x0.array() - tau, x);
+    //x = (x0.array() < -tau).select(x0.array() + tau, x);
+    Eigen::VectorXd x = x0;
+    for (int i = 0; i < int(x0.size()); ++i) {
+        if (x[i] < -tau) {
+            x[i] += tau;
+        } else if (x[i] > tau) {
+            x[i] -= tau;
+        } else {
+            x[i] = 0;
+        }
+    }
     return x;
 }
